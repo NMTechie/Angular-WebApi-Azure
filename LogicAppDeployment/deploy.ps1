d102d8d7a9cb3f9dedcbebcc7fe14b56fa93fca8
NMTechie <nileshranjan.mitra@yahoo.in>
2018-07-23 14:11:13 +0530

    Logic App power shell script

---
+++ LogicAppDeployment/deploy.ps1
@@ -0,0 +1,162 @@
+<#
+ .SYNOPSIS
+    Deploys a template to Azure
+
+ .DESCRIPTION
+    Deploys an Azure Resource Manager template
+
+ .PARAMETER subscriptionId
+    The subscription id where the template will be deployed.
+
+ .PARAMETER resourceGroupName
+    The resource group where the template will be deployed. Can be the name of an existing or a new resource group.
+
+ .PARAMETER resourceGroupLocation
+    Optional, a resource group location. If specified, will try to create a new resource group in this location. If not specified, assumes resource group is existing.
+
+ .PARAMETER deploymentName
+    The deployment name.
+
+ .PARAMETER templateFilePath
+    Optional, path to the template file. Defaults to template.json.
+
+ .PARAMETER parametersFilePath
+    Optional, path to the parameters file. Defaults to parameters.json. If file is not found, will prompt for parameter values based on template.
+#>
+
+param(
+ [Parameter(Mandatory=$True)]
+ [string]
+ $subscriptionId,
+
+ [Parameter(Mandatory=$True)]
+ [string]
+ $resourceGroupName,
+
+ [string]
+ $resourceGroupLocation,
+
+ [Parameter(Mandatory=$True)]
+ [string]
+ $deploymentName,
+
+ [string]
+ $templateFilePath = "template.json",
+
+ [string]
+ $parametersFilePath = "parameters.json"
+)
+
+<#
+.SYNOPSIS
+    Registers RPs
+#>
+Function RegisterRP {
+    Param(
+        [string]$ResourceProviderNamespace
+    )
+
+    Write-Host "Registering resource provider '$ResourceProviderNamespace'";
+    Register-AzureRmResourceProvider -ProviderNamespace $ResourceProviderNamespace;
+}
+
+#region mini window, made by Scripting Guy Blog
+    Function Show-OAuthWindow {
+    Add-Type -AssemblyName System.Windows.Forms
+ 
+    $form = New-Object -TypeName System.Windows.Forms.Form -Property @{Width=600;Height=800}
+    $web  = New-Object -TypeName System.Windows.Forms.WebBrowser -Property @{Width=580;Height=780;Url=($url -f ($Scope -join "%20")) }
+    $DocComp  = {
+            $Global:uri = $web.Url.AbsoluteUri
+            if ($Global:Uri -match "error=[^&]*|code=[^&]*") {$form.Close() }
+    }
+    $web.ScriptErrorsSuppressed = $true
+    $web.Add_DocumentCompleted($DocComp)
+    $form.Controls.Add($web)
+    $form.Add_Shown({$form.Activate()})
+    $form.ShowDialog() | Out-Null
+    }
+    #endregion
+
+#******************************************************************************
+# Script body
+# Execution begins here
+#******************************************************************************
+$ErrorActionPreference = "Stop"
+
+# sign in
+Write-Host "Logging in...";
+Login-AzureRmAccount;
+
+# select subscription
+Write-Host "Selecting subscription '$subscriptionId'";
+$subscription = Select-AzureRmSubscription -SubscriptionID $subscriptionId;
+
+# Register RPs
+$resourceProviders = @("microsoft.cache","microsoft.compute","microsoft.eventgrid","microsoft.insights","microsoft.logic","microsoft.network","microsoft.sql","microsoft.storage","microsoft.web");
+if($resourceProviders.length) {
+    Write-Host "Registering resource providers"
+    foreach($resourceProvider in $resourceProviders) {
+        RegisterRP($resourceProvider);
+    }
+}
+
+#Create or check for existing resource group
+$resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
+if(!$resourceGroup)
+{
+    Write-Host "Resource group '$resourceGroupName' does not exist. To create a new resource group, please enter a location.";
+    if(!$resourceGroupLocation) {
+        $resourceGroupLocation = Read-Host "resourceGroupLocation";
+    }
+    Write-Host "Creating resource group '$resourceGroupName' in location '$resourceGroupLocation'";
+    New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation
+}
+else{
+    Write-Host "Using existing resource group '$resourceGroupName'";
+}
+
+# Start the deployment
+Write-Host "Starting deployment...";
+if(Test-Path $parametersFilePath) {
+    New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -TemplateParameterFile $parametersFilePath;
+} else {
+    New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath;
+}
+
+#start the Authorizing the Connection
+$connection = Get-AzureRmResource -ResourceType "Microsoft.Web/connections" -ResourceGroupName $resourceGroupName -ResourceName "azureeventgrid"
+
+# Write-Host "connection status: " $connection.Properties.Statuses[0]
+
+$parameters = @{
+	"parameters" = ,@{
+	"parameterName"= "token";
+	"redirectUrl"= "https://ema1.exp.azure.com/ema/default/authredirect"
+	}
+}
+
+#get the links needed for consent
+$consentResponse = Invoke-AzureRmResourceAction -Action "listConsentLinks" -ResourceId $connection.ResourceId -Parameters $parameters -Force
+
+$url = $consentResponse.Value.Link 
+
+#prompt user to login and grab the code after auth
+Show-OAuthWindow -URL $url
+
+$regex = '(code=)(.*)$'
+    $code  = ($uri | Select-string -pattern $regex).Matches[0].Groups[2].Value
+    Write-output "Received an accessCode: $code"
+
+if (-Not [string]::IsNullOrEmpty($code)) {
+	$parameters = @{ }
+	$parameters.Add("code", $code)
+	# NOTE: errors ignored as this appears to error due to a null response
+
+    #confirm the consent code
+	Invoke-AzureRmResourceAction -Action "confirmConsentCode" -ResourceId $connection.ResourceId -Parameters $parameters -Force -ErrorAction Ignore
+}
+
+#retrieve the connection
+$connection = Get-AzureRmResource -ResourceType "Microsoft.Web/connections" -ResourceGroupName $ResourceGroupName -ResourceName "azureeventgrid"
+# Write-Host "connection status now: " $connection.Properties.Statuses[0]
